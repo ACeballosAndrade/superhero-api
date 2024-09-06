b43548c1d54a72dfebd96e8b8c7b5533b99a65ad
ACeballosAndrade <anthony.ceballos00@usc.edu.co>
2024-09-06 16:26:19 -0500

    settings

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: 'https://ACeballosAndrade.github.io/superhero-api/',
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://superheroapi.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      }
-    }
-  }
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'https://superheroapi.com',
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, ''),
+  //     }
+  //   }
+  // }
 })
