2194e3dbee1e02ebe3e9df43de43000704279333
ACeballosAndrade <anthony.ceballos00@usc.edu.co>
2024-09-06 15:33:56 -0500

    updating

--- src/hooks/useSuperheores.tsx
+++ src/hooks/useSuperheores.tsx
@@ -1,17 +1,60 @@
 import {  useState } from "react";
 import {getSuperHeroe, searchSuperheroes} from '../services/superheroes'
+import { Result } from "../types";
 //import debounce from "just-debounce-it";
 
 interface Props {
   query: string
 }
 
+const initialValue: Result = {
+  id: "",
+  name: "",
+  powerstats: {
+    intelligence: "",
+    strength: "",
+    speed: "",
+    durability: "",
+    power: "",
+    combat: ""
+  },
+  biography: {
+    "full-name": "",
+    "alter-egos": "",
+    aliases: [],
+    "place-of-birth": "",
+    "first-appearance": "",
+    publisher: "",
+    alignment: ""
+  },
+  appearance: {
+    gender: "",
+    race: "",
+    height: ["", ""], // Suponiendo que tenga dos valores, puedes cambiarlo si es necesario
+    weight: ["", ""],
+    "eye-color": "",
+    "hair-color": ""
+  },
+  work: {
+    occupation: "",
+    base: ""
+  },
+  connections: {
+    "group-affiliation": "",
+    relatives: ""
+  },
+  image: {
+    url: ""
+  }
+};
+
+
 export function useSuperheores() {
   
   const [response, setResponse] = useState([]);
-  const [ superHero, setSuperHero ] = useState({})
+  const [ superHero, setSuperHero ] = useState(initialValue)
   const [ loading, setLoading ] = useState(false)
-  const [ error, setError ] = useState(null)
+  const [ error, setError ] = useState('')
 
   
 
@@ -22,7 +65,7 @@ export function useSuperheores() {
       setResponse(superHeroes)
       setLoading(false)
     } catch (error) {
-      setError(error.message)
+      setError((error as Error).message)
       setLoading(false)
     }
   };
